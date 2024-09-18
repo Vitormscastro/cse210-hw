610bec8be915a91e784289bd1b8cedddec79cc69
Vitormscastro <vitormscastro2022@gmail.com>
2024-09-18 19:13:51 -0300

    Prep2 program complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade percentage: ");
+        string string_grade = Console.ReadLine();
+        int grade = int.Parse(string_grade);
+        string grade_letter = "";
+        string sign = "";
+        string pass = "";
+
+        if (grade >= 90)
+        {
+            grade_letter = "A";
+            if(grade <= 93)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            grade_letter = "B";
+            if (grade >= 87)
+            {
+                sign = "+";
+            }
+            if(grade <= 83)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 70)
+        {
+            grade_letter = "C";
+            if (grade >= 77)
+            {
+                sign = "+";
+            }
+            if(grade <= 73)
+            {
+                sign = "-";
+            }
+        }
+        else if (grade >= 60)
+        {
+            grade_letter = "D";
+            if (grade >= 67)
+            {
+                sign = "+";
+            }
+            if(grade <= 63)
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            grade_letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            pass = "You passed! ";
+        }
+        else
+        {
+            pass = "You'll get it next semester!"; 
+        }
+
+        Console.WriteLine($"{pass}You received the following grade: {grade_letter}{sign}");
     }
 }
